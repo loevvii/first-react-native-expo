ab19a1731f1a86d6840d78c9f95359184d146ffa
loevvii <lhvinas112203@gmail.com>
2025-03-01 11:48:23 +0800

    thing

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,80 +1,20 @@
-import React, { createContext, useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, Switch, SafeAreaView, Animated } from 'react-native';
-import Projects from './src/pages/Projects';
-import Profile from './src/pages/Profile';
-
-interface ThemeContextType {  
-  darkMode: boolean;
-  setDarkMode: (mode: boolean) => void;
-}
-
-export const ThemeContext = createContext<ThemeContextType>({ 
-  darkMode: false,
-  setDarkMode: () => {},
-});
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [darkMode, setDarkMode] = useState(false);
-  const backgroundColor = useRef(new Animated.Value(0)).current;
-  const textColor = useRef(new Animated.Value(0)).current;
-
-  useEffect(() => {
-    Animated.timing(backgroundColor, {
-      toValue: darkMode ? 1 : 0,
-      duration: 500,
-      useNativeDriver: false,
-    }).start();
-    
-    Animated.timing(textColor, {
-      toValue: darkMode ? 1 : 0,
-      duration: 500,
-      useNativeDriver: false,
-    }).start();
-  }, [darkMode]);
-
-  const interpolatedBackgroundColor = backgroundColor.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['#faf4ed', '#232136'], // Dawn -> Moon
-  });
-
-  const interpolatedTextColor = textColor.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['#575279', '#e0def4'], // Dawn -> Moon
-  });
-
   return (
-    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
-      <Animated.View style={[styles.container, { backgroundColor: interpolatedBackgroundColor }]}>  
-        <Profile />
-        <Animated.Text style={[styles.header, { color: interpolatedTextColor }]}>My Projects</Animated.Text>
-        <Projects />
-        <Animated.Text style={[styles.text, { color: interpolatedTextColor }]}>
-          {darkMode ? "Light Mode" : "Dark Mode"}
-        </Animated.Text>
-        <Switch 
-          value={darkMode} 
-          onValueChange={() => setDarkMode(!darkMode)} 
-          thumbColor={darkMode ? "#c4a7e7" : "#907aa9"} 
-          trackColor={{ false: "#575279", true: "#c4a7e7" }}
-        />
-      </Animated.View>
-    </ThemeContext.Provider>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 20,
   },
-  text: {
-    fontSize: 20,
-    marginBottom: 10,
-  },
-  header: {
-    fontSize: 30,
-    fontWeight: 'bold',
-  }
 });
